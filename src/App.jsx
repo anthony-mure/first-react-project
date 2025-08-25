a69a0f76ee8763032f9fbbd3f1197a9059362213
anthony-murè <tony.mure99@gmail.com>
2025-08-25 18:45:29 +0200

    [add]Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 //IMPORTO LE COMPONENTI
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
      <Header />
      <Main />
+     <Footer />
     </>
   );
-}
+};
 
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,34 @@
 const Footer = () => {
   return (
     <footer>
+      <div>
+        
+        <div>
+          <img src="/img/buy-comics-digital-comics.png" alt="logo dc"></img>
+          <p>DIGITAL COMICS</p>
+        </div>
+
+        <div>
+          <img src="/img/buy-comics-merchandise.png" alt="dc merchandise"></img>
+          <p>DC MERCHANDISE</p>
+        </div>
+
+        <div>
+          <img src="/img/buy-comics-subscriptions.png" alt="subscription"></img>
+          <p>SUBSCRIPTION</p>
+        </div>
+
+        <div>
+          <img src="/img/buy-comics-shop-locator.png" alt="comic shop locator"></img>
+          <p>COMIC SHOP LOCATOR</p>
+        </div>
+
+        <div>
+          <img src="/img/buy-dc-power-visa.svg" alt="dc power visa"></img>
+          <p>DC POWER VISA</p>
+        </div>
+
+      </div>
       
     </footer>
   )
