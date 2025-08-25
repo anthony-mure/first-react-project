3806ca8a0b2a31dfc631a7715fcbb54b54679c9f
anthony-murè <tony.mure99@gmail.com>
2025-08-25 20:21:26 +0200

    [add]footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,9 +31,86 @@ const Footer = () => {
         </div>
 
       </div>
+
+      <div>
+          <div>
+            <h4>DC COMICS</h4>
+           <ul>
+             <li><a href="#">Characters</a></li>
+             <li><a href="#">Comics</a></li>
+             <li><a href="#">Movies</a></li>
+             <li><a href="#">Tv</a></li>
+             <li><a href="#">Games</a></li>
+             <li><a href="#">Videos</a></li>
+             <li><a href="#">News</a></li>
+           </ul>
+          </div>
+
+          <div>
+            <h4>SHOP</h4>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>DC</h4>
+            <ul>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Privacy Policy(new)</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent Workshops</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>SITES</h4>
+            <ul>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </div>
+
+      </div>
+
+      <div>
+        <div>
+          <button>SING-UP NOW!</button>
+        </div>
+        <div>
+          <span>FOLLOW US</span>
+          <img src="/img/footer-facebook.png" alt="facebook"></img>
+          <img src="/img/footer-twitter.png" alt="twitter"></img>
+          <img src="/img/footer-youtube.png" alt="youtube"></img>
+          <img src="/img/footer-pinterest.png" alt="pinterest"></img>
+          <img src="/img/footer-periscope.png" alt="periscope"></img>
+        </div>
+        
+      </div>
+          
+
+
+
+      
+
+
+
+      
+      
       
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
