9163a3e4ba8ed488a9e7e9373445a258f99d569b
anthony-murè <tony.mure99@gmail.com>
2025-08-28 09:07:35 +0200

    modified ComicCard.jsx

--- src/components/ComicCard.jsx
+++ src/components/ComicCard.jsx
@@ -4,8 +4,8 @@ const ComicCard = ({id,title,thumb}) => {
   return (
     <div className="col-15">
       <div className="comic-card">
-        <img src={comic.thumb} alt={comic.title} />
-        <h4>{comic.title}</h4>
+        <img src={thumb} alt={title} />
+        <h4>{title}</h4>
       </div>
     </div>
   );
